6aa956edc560f3c58f68870f74bda11eabaa0178
Kholmumin Tursinboev <kholmumintursinboev@gmail.com>
2022-04-19 13:19:04 +0500

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Kholmumin Tursinboev on 19/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
